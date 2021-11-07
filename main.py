0a60412e54848c7d9715bb93a585c2d0366d1456
aymendps <hammami.aym@outlook.com>
2021-11-07 12:35:36 +0100

    Finished main window GUI?

--- main.py
+++ main.py
@@ -2,12 +2,48 @@ import mywindow as mw
 
 # main_window parameters
 width = 400
-height = 500
+height = 550
 title = "Job Seeker - Apply Right"
 logo_path = "./assets/logo.png"
 bg_hex_color = '#eb1087'
+text_color = 'white'
+websites = ["RealPython JobSite", "LinkedIn", "Tanit Jobs"]
+websites_title = "Website to use:"
+methods = ["Seek everything", "Seek a category", "Seek a keyword"]
+methods_title = "Method to use:"
+categories = ["Software & Data", "Science", "Engineering", "Sales & Marketing", "Healthcare", "Business",
+              "Education", "Arts & Media", "Technology", "Social Services", "Construction"]
+categories_title = "Categories / Fields:"
+keyword_title = "Seek Keyword:"
+seek_button_text = "Seek now!"
 
 if __name__ == '__main__':
-    main_window = mw.myWindow(w=width, h=height, title=title, bg_hex_color=bg_hex_color)
+    main_window = mw.myWindow(w=width, h=height, title=title, bg_hex_color=bg_hex_color, text_color=text_color)
+
     main_window.add_image(logo_path)
+
+    text0 = main_window.add_text(websites_title, 'w')
+    mw.add_padding(text0, 20, 5)
+
+    dropdown0 = main_window.add_dropdown(websites, 45)
+
+    text1 = main_window.add_text(methods_title, 'w')
+    mw.add_padding(text1, 20, 5)
+
+    dropdown1, d1_variable = main_window.add_dropdown(methods, 45)
+
+    text2 = main_window.add_text(categories_title, 'w')
+    mw.add_padding(text2, 20, 5)
+
+    dropdown2 = main_window.add_dropdown(categories, 45)
+
+    text3 = main_window.add_text(keyword_title, 'w')
+    mw.add_padding(text3, 20, 5)
+
+    field0 = main_window.add_input_field(width=39)
+
+    button0 = main_window.add_button(seek_button_text)
+    mw.add_padding(button0, 10, 10)
+    mw.set_position(button0, 150, 480)
+
     main_window.start_window()
--- mywindow.py
+++ mywindow.py
@@ -1,14 +1,16 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+
 class myWindow:
     window = tk.Tk()
 
-    def __init__(self, w, h, title,bg_hex_color):
+    def __init__(self, w, h, title, bg_hex_color, text_color):
         self.w = w
         self.h = h
         self.title = title
         self.bg_hex_color = bg_hex_color
+        self.text_color = text_color
         self.create_window()
 
     def start_window(self):
@@ -23,6 +25,11 @@ class myWindow:
         self.window.resizable(False, False)
         self.window['background'] = self.bg_hex_color
 
+    def get_window_position(self):
+        pos_w = int(self.window.winfo_screenwidth() / 2 - self.w / 2)
+        pos_h = int(self.window.winfo_screenheight() / 2 - self.h / 2)
+        return pos_w, pos_h
+
     def add_image(self, image_path):
         # tk.Label(self.window, text="Username").place(x=40, y=60)
         data = Image.open(image_path)
@@ -30,3 +37,45 @@ class myWindow:
         my_label = tk.Label(self.window, image=image, bg=self.bg_hex_color)
         my_label.image = image
         my_label.pack()
+
+    def add_text(self, some_text, anchor):  # anchor? align text: 'w' => right 'e' => left 'center' => center
+        my_text = tk.Label(self.window, text=some_text, bg=self.bg_hex_color, fg=self.text_color,
+                           font=("Arial", 12, "bold"), anchor=anchor)
+        my_text.pack(fill='both')
+        return my_text
+
+    def add_dropdown(self, my_list, width, command=None):
+        var = tk.StringVar(self.window)
+        var.set(my_list[0])
+        dropdown = tk.OptionMenu(self.window, var, *my_list)
+        dropdown.config(bg=self.bg_hex_color, fg=self.text_color, font=("Arial", 10, "bold"),
+                        activeforeground=self.bg_hex_color, width=width, direction='right')
+        if command is not None:
+            dropdown.config(command=command)
+
+        dropdown["menu"].configure(bg=self.bg_hex_color, activebackground='#4c1b45', fg=self.text_color)
+        dropdown.pack()
+        return dropdown, var
+
+    def add_input_field(self, width):
+        field = tk.Entry(self.window, width=width, font=("Arial", 12, "bold"), fg='#4c1b45')
+        field.pack()
+        return field
+
+    def add_button(self, button_text):
+        button = tk.Button(self.window, text=button_text, bg=self.bg_hex_color, font=("Arial", 10, "bold"),
+                           fg=self.text_color, activeforeground=self.bg_hex_color, relief="groove", bd=5)
+        button.pack()
+        return button
+
+
+def set_position(my_object, x, y):
+    my_object.place(x=x, y=y)
+
+
+def add_padding(my_object, padx, pady):
+    my_object.config(padx=padx, pady=pady)
+
+
+def disable_object(my_object):
+    my_object.config(state="disabled")
